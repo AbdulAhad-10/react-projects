3faa1c21cdc33b0f47062b0055d41ae7dea26081
Abdul Ahad <ahad55056@gmail.com>
2023-12-31 16:04:26 +0500

    Waygo commit

--- Waygo/src/components/navbar/Navbar.jsx
+++ Waygo/src/components/navbar/Navbar.jsx
@@ -1,41 +1,41 @@
-import logo from '../../assets/logo.svg'
-import menu from '../../assets/menu.svg'
-import close from '../../assets/close.svg'
-import { navLinks } from '../../constants'
-import './navbar.css'
-import { useState } from 'react'
+import logo from "../../assets/logo.svg";
+import menu from "../../assets/menu.svg";
+import close from "../../assets/close.svg";
+import { navLinks } from "../../constants";
+import "./navbar.css";
+import { useState } from "react";
 
 const Navbar = () => {
+  const [toggle, setToggle] = useState(true);
 
-  const [toggle, setToggle] = useState(true)
-
-  const navItems = navLinks.map(link => (
+  const navItems = navLinks.map((link) => (
     <li key={link.label}>
       <a href={link.href}>{link.label}</a>
     </li>
-  ))
+  ));
 
   return (
-    <nav className='nav'>
+    <nav className="nav">
+      <img src={logo} alt="Logo" />
 
-        <img src={logo} alt="Logo" />
-    
-        <ul  className={toggle ? 'nav__links' : 'nav__links display_dropdown'}>
-          {navItems}
-          <li><button className='nav__login'>Login</button></li>
-          <li>
-          <button className='button'>Register</button>
-          </li>
-        </ul>
+      <ul className={toggle ? "nav__links" : "nav__links display_dropdown"}>
+        {navItems}
+        <li>
+          <button className="nav__login">Login</button>
+        </li>
+        <li>
+          <button className="button">Register</button>
+        </li>
+      </ul>
 
-      <img className='nav__menu-toggle' src={toggle ? menu : close} alt="" onClick={() => setToggle(prevtoggle => !prevtoggle)}/>
-
-        
-        
-        
-
+      <img
+        className="nav__menu-toggle"
+        src={toggle ? menu : close}
+        alt=""
+        onClick={() => setToggle((prevtoggle) => !prevtoggle)}
+      />
     </nav>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
